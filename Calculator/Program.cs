0fa09ecc39242a8788d8a3487f5d001fe6f069ec
yangajila <griangajila2@gmail.com>
2023-03-27 15:43:45 +0800

    function: saving operation and show history done

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,5 +1,4 @@
-﻿
-using Calculator.Math;
+﻿using Calculator.Math;
 
 namespace Calculator;
 
@@ -16,92 +15,63 @@ public static class Program
     private static void Main(string[] args)
     {
         var cmd = args[0];
+        var db = Environment.ExpandEnvironmentVariables("%USERPROFILE%\\Desktop\\History.txt");
         switch (cmd)
         {
             case "show":
-                ShowHistory();
+                ShowHistory(db);
                 return;
             case "remove":
-                RemoveHistory();
+                RemoveHistory(db);
                 return;
         }
-
-        var db = Environment.ExpandEnvironmentVariables("%USERPROFILE%\\Desktop\\History.txt");
-        var history = new List<string>();
-        if (File.Exists(db))
-        {
-            history.AddRange(File.ReadAllLines(db));
-            return;
-            
-        } 
+        
         if (!Operators.TryGetValue(cmd, out var op))
         {
             Console.WriteLine($"Invalid operator: {cmd}");
             return;
         }
+
         var numbers = new double[args.Length - 1];
         for (var i = 1; i < args.Length; i++)
         {
             if (double.TryParse(args[i], out var number))
             {
-                numbers[i -1] = number;
+                numbers[i -1] =number;
             }
             else
             {
                 Console.WriteLine($"Invalid type of input: {args[i]}");
             }
         }
-        
-        if (numbers.Length < 2)
-        {
-            return;
-        }
-        
         var result = op.Operation.Calculate(numbers.ToArray());
-        Console.WriteLine($"Here is the result: {result}");
         // 1 + 1 = 2
-        var file = "";
-        foreach (var valNumber in numbers)
-        {
-            file += $"{string.Join(" " + op.Symbol + " ", valNumber)}";
-        }
-        file = file.Substring(0, file.Length - 1);
+        var file = $"{string.Join(" "+op.Symbol + " ", numbers)}";
         file += $" = {result}";
-        SaveHistory(file);
-
-
-    }
-    //save history
-    private static void SaveHistory(string operation)
-    {
-        var db = Environment.ExpandEnvironmentVariables("%USERPROFILE%\\Desktop\\History.txt");
-        using var filePath = File.AppendText(db);
-        filePath.WriteLine(operation);
-    }
-    //show history
-    private static void ShowHistory()
-    {
-        var db = Environment.ExpandEnvironmentVariables("%USERPROFILE%\\Desktop\\History.txt");
-        if (!File.Exists(db)) Console.WriteLine("History not found!");
-        Console.WriteLine("History:");
-        var history = File.ReadAllLines(db);
-        foreach (var operation in history)
-        {
-            Console.WriteLine(operation);
-        }
+        Console.WriteLine($"Here is the result: {result}");
+        File.AppendAllText(db, $"{file}\n");
     }
     
-    //remove history
-    private static void RemoveHistory()
+    
+    //show history
+    private static void ShowHistory(string filePath)
     {
-        var db = Environment.ExpandEnvironmentVariables("%USERPROFILE%\\Desktop\\History.txt");
-        //invert if statement
-        if (!File.Exists(db)) Console.WriteLine("History already removed!");
-        File.Delete(db);
-        Console.WriteLine("History Cleared.");
+        if (!File.Exists(filePath)) return;
+        Console.WriteLine("History:");
+        var history = File.ReadAllText(filePath);
+        Console.WriteLine(history);
+    }
+    //remove history
+    private static void RemoveHistory(string filePath)
+    {
+        if (!File.Exists(filePath))
+        {
+            Console.WriteLine("History already removed");
+        }
+        File.Delete(filePath);
+        Console.WriteLine("History Cleared");
     }
 }
-
 internal class Operator
 {
     public char Symbol { get;}
