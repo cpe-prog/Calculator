589effa3971133dbfaddcb6c34f635312fafbda1
grian <griangajila2@gmail.com>
2023-04-18 21:13:08 +0800

    Refactoring Test Method

--- Calculators.Tests/UnitTest1.cs
+++ Calculators.Tests/UnitTest1.cs
@@ -3,18 +3,14 @@ using Xunit;
 
 namespace Calculators.Tests
 {
-    public class AdditionCalculationTests
+    public class CalculationTests
     {
         [Theory]
         [InlineData(new double[] { 2, 3, 10 }, 15)]
-        [InlineData(new double[] { -2, 3 }, 1)]
-        [InlineData(new double[] { 2, -3 }, -1)]
-        [InlineData(new double[] { -2, -3 }, -5)]
-        [InlineData(new double[] { 2, 3 }, 5)]
-        public void TestAddCalculate(double[] numbers, double expected)
+        public void AddCalculationTest(double[] numbers, double expected)
         {
 
-            var addOperator = new AddOperation();
+            var addOperator = new AddOperation(); 
             var addResult = addOperator.Calculate(numbers.ToArray());
 
             switch (addOperator.Symbol)
@@ -25,17 +21,10 @@ namespace Calculators.Tests
             }
 
         }
-    }
-
-    public class SubtractionCalculationTests
-    {
+        
         [Theory]
-        [InlineData(new double[] { -5, 5}, 0)] 
-        [InlineData(new double[] { -2, 3 }, 1)]
-        [InlineData(new double[] { 2, -3 }, -1)]
-        [InlineData(new double[] { -2, -3 }, -5)] 
-        [InlineData(new double[] { -2, 8 }, 6)]
-        public void TestSubtractCalculate(double[] numbers, double expected)
+        [InlineData(new double[] { -5, 5}, 0)]
+        public void SubtractCalculationTest(double[] numbers, double expected)
         {
             var subtractOperator = new SubtractOperation();
             var subtractResult = subtractOperator.Calculate(numbers.ToArray());
@@ -46,47 +35,35 @@ namespace Calculators.Tests
                     break;
             }
         }
+        
+        [Theory]
+        [InlineData(new double[] { 5, 5}, 25)]
+        public void MultiplyCalculationTest(double[] numbers, double expected)
+        {
+            var multiplyOperator = new MultiplyOperation();
+            var multiplyResult = multiplyOperator.Calculate(numbers.ToArray());
+            switch (multiplyOperator.Symbol)
+            {
+                case '×':
+                    Assert.Equal(expected, multiplyResult);
+                    break;
+            }
+        }
+        
+        [Theory]
+        [InlineData(new double[] { 20, 2}, 30)]
+        public void DivideCalculationTest(double[] numbers, double expected)
+        {
+            var divideOperator = new DivideOperation();
+            var divideResult = divideOperator.Calculate(numbers.ToArray());
+            switch (divideOperator.Symbol)
+            {
+                case '÷':
+                    Assert.Equal(expected, divideResult);
+                    break;
+            }
+        }
+        
+        
     }
-
-   public class MultiplicationCalculationTests
-   {
-       [Theory]
-       [InlineData(new double[] { 5, 5}, 25)] 
-       [InlineData(new double[] { 2, 3 }, 6)]
-       [InlineData(new double[] { 3, 3 }, 9)]
-       [InlineData(new double[] { -2, -3 }, -5)] 
-       [InlineData(new double[] { 10, 3 }, 30)]
-       public void TestMultiplyCalculate(double[] numbers, double expected)
-       {
-           var multiplyOperator = new MultiplyOperation();
-           var multiplyResult = multiplyOperator.Calculate(numbers.ToArray());
-           switch (multiplyOperator.Symbol)
-           {
-               case '×':
-                   Assert.Equal(expected, multiplyResult);
-                   break;
-           }
-       }
-   }
-   
-   public class DivisionCalculationTests
-   {
-       [Theory]
-       [InlineData(new double[] { 20, 2}, 30)]
-       [InlineData(new double[] { 20, 10 }, 2)]
-       [InlineData(new double[] { 50, 10 }, 5)]
-       [InlineData(new double[] { 10, 10 }, 1)]
-       [InlineData(new double[] { 0, 0 }, 0)]
-       public void TestDivideCalculate(double[] numbers, double expected)
-       {
-           var divideOperator = new DivideOperation();
-           var divideResult = divideOperator.Calculate(numbers.ToArray());
-           switch (divideOperator.Symbol)
-           {
-               case '÷':
-                   Assert.Equal(expected, divideResult);
-                   break;
-           }
-       }
-   }
 }
